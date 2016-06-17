bfc64e6f82fc5c2224c8f991a6d7f1c80d3a5f95
pnakibar <pedronakibar@gmail.com>
2016-06-17 18:57:23 -0300

    working navigator!

--- index.android.js
+++ index.android.js
@@ -17,23 +17,35 @@ import {
   TouchableOpacity
 } from 'react-native';
 
-const Main = () => (
-  <View style={styles.container}>
-    <Text>
-      Main!
-    </Text>
-  </View>
-)
-
-const Home = () => (
-    <Text>
-      Home!
-    </Text>
-)
-
-const TopBar = ({navigator, navState}) => (
-  <Text> AUSHDUAHSUHDUHD </Text>
-)
+class AwesomeProject extends Component {
+  render() {
+      return (
+        <Navigator
+            renderScene={this.renderScene.bind(this)}
+            navigator={this.props.navigator}
+            navigationBar={
+              <Navigator.NavigationBar style={{backgroundColor: '#246dd5'}}
+                  routeMapper={NavigationBarRouteMapper} />
+            } />
+      );
+    }
+    renderScene(route, navigator) {
+      return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
+          <TouchableHighlight style={{backgroundColor: 'yellow', padding: 10}}
+              onPress={this.gotoPersonPage.bind(this)}>
+            <Text style={{backgroundColor: 'yellow', color: 'green'}}>Center</Text>
+          </TouchableHighlight>
+        </View>
+      );
+    }
+    gotoPersonPage() {
+      this.props.navigator.push({
+        id: 'PersonPage',
+        name: '我的主页',
+      });
+    }
+}
 
 var NavigationBarRouteMapper = {
   LeftButton(route, navigator, index, navState) {
@@ -41,7 +53,7 @@ var NavigationBarRouteMapper = {
       <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}
           onPress={() => navigator.parentNavigator.pop()}>
         <Text style={{color: 'white', margin: 10,}}>
-          返回
+          Back
         </Text>
       </TouchableOpacity>
     );
@@ -53,63 +65,11 @@ var NavigationBarRouteMapper = {
     return (
       <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}>
         <Text style={{color: 'white', margin: 10, fontSize: 16}}>
-          主页
+          Title
         </Text>
       </TouchableOpacity>
     );
   }
 };
-
-class AwesomeProject extends Component {
-  render() {
-    return (
-      <Navigator
-        initialRoute={{id: 'Home', name: 'Index'}}
-        renderScene={this.renderScene.bind(this)}
-        configureScene={(route) => {
-          if (route.sceneConfig) {
-            return route.sceneConfig;
-          }
-          return Navigator.SceneConfigs.FloatFromRight;
-        }}
-        navigationBar={
-          <Navigator.NavigationBar
-            routeMapper={NavigationBarRouteMapper}
-          />
-        }
-      />
-    );
-   }
-  renderScene(route, navigator) {
-    var routeId = route.id;
-    switch (route.id) {
-      case ('Home'):
-        return <Home navigator={navigator} />
-    }
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  icon: {
-    width: 100,
-    height: 100,
-  },
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
