6686c776bf6aa3d78a37f231b63b84f60bc30126
pnakibar <pedronakibar@gmail.com>
2016-06-17 23:34:25 -0300

    cleaning up index.android.js: removing old DaySelector code

--- index.android.js
+++ index.android.js
@@ -29,43 +29,7 @@ const beforeToday = numberOfDays.map((x) => moment(today).subtract(x, 'days')).r
 const afterToday = numberOfDays.map((x) => moment(today).add(x, 'days'));
 const THUMBS = [...beforeToday, today, ...afterToday]
 
-const Thumb = ({ text }) => {
-  const formatedDate = text.format('ddd D').split(' ');
-  const [dayText, dayNumber] = formatedDate;
-  const isToday = moment().startOf('day').diff(moment(text).startOf('day')) === 0;
-  const boxStyle = isToday ?
-    [styles.button, styles.buttonSelected] :
-    styles.button;
-  const textStyle = isToday ?
-    [styles.buttonText, styles.buttonSelectedText] :
-    styles.buttonText;
 
-  return (
-    <View style={boxStyle}>
-      <Text style={textStyle}>
-        {dayText.toUpperCase()}
-      </Text>
-      <Text style={textStyle}>
-        {dayNumber}
-      </Text>
-    </View>
-  )
-};
-
-const HorizontalTable = ({ texts }) => {
-  var _scrollView: ScrollView;
-  return (
-    <View >
-      <ScrollView
-        ref={(scrollView) => { _scrollView = scrollView; }}
-        horizontal={true}
-        scrollEventThrottle={200}
-        style={styles.scrollView}>
-        {texts.map((c, i) => createThumbRow(c, i))}
-      </ScrollView>
-    </View>
-  );
-}
 class AwesomeProject extends Component {
   render() {
       return (
@@ -114,33 +78,6 @@ var NavigationBarRouteMapper = {
 };
 
 var styles = StyleSheet.create({
-  buttonSelectedText: {
-    color: "#FFF",
-  },
-  scrollView: {
-    alignSelf: "stretch",
-    height: 140,
-    width: Dimensions.get('window').width,
-  },
-  button: {
-    justifyContent:'center',
-    paddingTop: 35,
-    paddingBottom: 40,
-    marginLeft: 1,
-    marginRight: 1,
-    padding: 5,
-    width: 140,
-    height: 140,
-    alignItems: 'center',
-    backgroundColor: '#E7E7E7',
-  },
-  buttonSelected: {
-    backgroundColor: '#CC0814',
-  },
-  img: {
-    width: 64,
-    height: 64,
-  },
 })
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
