aaff47a91eeaf4f4945931c97cffb0bb9d448dcd
pnakibar <pedronakibar@gmail.com>
2016-06-19 18:18:20 -0300

    removed unused code from AppNavigator.js

--- app/components/AppNavigator.js
+++ app/components/AppNavigator.js
@@ -1,34 +1,9 @@
 import React, { Component } from 'react';
 import { Router, Scene, Actions } from 'react-native-router-flux';
-import {
-  Text,
-  View,
-  TouchableHighlight,
-  BackAndroid
-} from 'react-native';
-
-/*
-BackAndroid.addEventListener('hardwareBackPress', function() {
-     if (!this.onMainScreen()) {
-       Actions.pop();
-       return true;
-     }
-    Actions.pop();
-    console.log(Actions)
-    return true
-});
-*/
+import { BackAndroid } from 'react-native';
 
 import ActivitiesListContainer from '../containers/ActivitiesListContainer'
 import ActivitySelectedContainer from '../containers/ActivitySelectedContainer'
-
-const Sample = () => (
-  <View>
-    <Text>
-      HI!
-    </Text>
-  </View>
-)
 
 export default class App extends Component {
   componentWillMount = () => {
